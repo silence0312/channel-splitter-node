68670fd7a92dfd873c9cb3281f65bb19018174b9
petpig <282407344@qq.com>
2017-06-02 10:30:50 +0800

    多板块可视化

---
+++ public/js/splitter.js
@@ -0,0 +1,101 @@
+
+	  /* $(document).ready(function() {
+	   	   $("btn").click(function() {
+	   		  $("audio").play();	   		
+	   	    });
+	      });
+      */
+	function a(){
+     var audio = document.getElementById('music1'); 
+     if(audio.paused){                 
+         audio.play();//audio.play();// 播放  
+     }
+     else{
+          audio.pause();// 暂停
+     } 
+    
+    }
+    function b(){
+     var audio = document.getElementById('music2'); 
+     if(audio.paused){                 
+         audio.play();//audio.play();// 播放  
+     }
+     else{
+          audio.pause();// 暂停
+     } 
+    }
+    function c(){
+     var audio = document.getElementById('music3'); 
+     if(audio.paused){                 
+         audio.play();//audio.play();// 播放  
+     }
+     else{
+          audio.pause();// 暂停
+     } 
+    
+    }
+    function d(){
+     var audio = document.getElementById('music4'); 
+     if(audio.paused){                 
+         audio.play();//audio.play();// 播放  
+     }
+     else{
+          audio.pause();// 暂停
+     } 
+    
+    }
+    function e(){
+     var audio = document.getElementById('music5'); 
+     if(audio.paused){                 
+         audio.play();//audio.play();// 播放  
+     }
+     else{
+          audio.pause();// 暂停
+     } 
+    
+    }
+    function f(){
+     var audio = document.getElementById('music6'); 
+     if(audio.paused){                 
+         audio.play();//audio.play();// 播放  
+     }
+     else{
+          audio.pause();// 暂停
+     } 
+    
+    }
+    function g(){
+     var audio = document.getElementById('music7'); 
+     if(audio.paused){                 
+         audio.play();//audio.play();// 播放  
+     }
+     else{
+          audio.pause();// 暂停
+     } 
+    
+    }
+    function h(){
+     var audio = document.getElementById('music8'); 
+     if(audio.paused){                 
+         audio.play();//audio.play();// 播放  
+     }
+     else{
+          audio.pause();// 暂停
+     } 
+    
+    }
+    function i(){
+     var audio = document.getElementById('music9'); 
+     if(audio.paused){                 
+         audio.play();//audio.play();// 播放  
+     }
+     else{
+          audio.pause();// 暂停
+     } 
+    
+    }
+
+
+
+
+
